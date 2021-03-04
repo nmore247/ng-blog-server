3d3581141e55387fd835ef77571bc8ff69740c6e
Nikhil More <nikhilmore2011@hotmail.com>
2021-03-04 11:53:01 +0100

    express server with cors policy

---
+++ mock-articles.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        id: 1,
+        title: 'My first article',
+        content: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque mollis ipsum ac semper. Suspendisse non pharetra nunc. Quisque commodo lectus ac urna dictum, vel sodales augue tristique. Duis euismod leo odio, ac tristique felis dictum ut. Phasellus vitae posuere nulla, eget gravida leo. Nulla luctus faucibus massa eu pretium. Integer vitae quam id urna pulvinar eleifend. Phasellus quis libero nec elit convallis laoreet ac id massa. Duis in vestibulum mi. Nulla a mollis dolor. Sed pulvinar scelerisque lacus at tincidunt. Ut risus massa, ultrices sit amet risus ut, gravida venenatis metus. Sed malesuada ac justo nec pellentesque.</p>",
+        description: 'This is my first article!',
+        key: 'my-first-article',
+        date: new Date(),
+        imageUrl: 'http://angular.io/assets/images/logos/angular/angular.png'
+    },
+    {
+        id: 2,
+        title: 'The second article',
+        content: "<p>Sed consectetur purus lorem, ac convallis mauris scelerisque id. Vestibulum dignissim diam et blandit euismod. Donec id tortor venenatis, gravida lectus in, tincidunt leo. Etiam metus lorem, pulvinar vitae ex at, pharetra volutpat mauris. Nullam sagittis erat sed imperdiet viverra. Maecenas quis felis at sem suscipit bibendum. Nunc suscipit aliquet odio feugiat finibus. Suspendisse ultrices nec orci et laoreet. Morbi at elit at nunc malesuada faucibus nec vitae elit. Etiam a tortor nec augue porttitor accumsan nec tristique odio. Etiam ligula nisi, maximus ut turpis varius, sollicitudin dapibus turpis.</p>",
+        description: 'Also a great article!',
+        key: 'my-second-article',
+        date: new Date(),
+        imageUrl: 'http://angular.io/assets/images/logos/angular/angular_solidBlack.png'
+    }
+
+]
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+    origin: ["http://localhost:4200", "http://localhost:4201"]
+}
+
+app.use(cors(corsOptions));
 
 app.listen(8000, ()=>{
     console.log("server is running.");
 })
+
+app.get("/", function(request, response) {
+    response.send("Hello Node.js");
+} )
+
+require("./articles.js")(app)
